e569eacfbc3e28d759079ec625eda7fc6581a88f
Sinimawath <sinimawath@gmail.com>
2018-08-29 01:18:30 +0300

    Upgrade/fix euclid func

--- source/euclid.js
+++ source/euclid.js
@@ -1,28 +1,26 @@
 'use strict';
-function euclid(){
-    if (arguments.length === 0) {
+const euclid = (...array) => {
+    if (array.length === 0) {
         return undefined;
     }
-    if (arguments.length === 1){
-        return arguments[0]
-    }
-    let gcd = 0;
-    let euclidFunc = function (a, b) {
-        a = Math.abs(a);
-        b = Math.abs(b);
-        while (a !== 0 && b !== 0){
-            if (a > b) {
-                a = a % b;
+    return array.reduce((gcd, curr) => {
+        if (typeof(curr) !== 'number') {
+            console.log("Incorrect: " + " curr: " + curr + " gcd:" + gcd);
+            return undefined;
+        }
+        gcd = Math.abs(gcd);
+        curr = Math.abs(curr);
+        while (gcd !== 0 && curr !== 0) {
+            if (gcd > curr) {
+                gcd = gcd % curr;
             }
-            else{
-                b = b % a;
+            else {
+                curr = curr % gcd;
             }
         }
-        return (a + b)
-    };
+        return (gcd + curr);
+    }, 0);
+};
 
-    for (let i = 1; i < arguments.length; i++){
-           gcd = euclidFunc(arguments[i], arguments[i-1]);
-    }
-    return gcd;
-}
+
+// console.log(euclid('a', 2));
